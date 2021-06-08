6850cc6aab2e8da4732b476105c7067c33e4a975
CuteTN <quantiennghia001@gmail.com>
2021-06-08 17:48:08 +0700

    Fetch list topic in Play screen

--- AnnaEnglish/Screens/PlayScreen/PlayScreen.js
+++ AnnaEnglish/Screens/PlayScreen/PlayScreen.js
@@ -1,8 +1,15 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import { Text, View } from "react-native";
+import { useFiredux } from '../../hooks/useFiredux';
 import * as styles from '../../shared/styles'
 
 function PlayScreen() {
+  const listTopics = Object.values(useFiredux("topic"));
+
+  useEffect(() => {
+    console.log(listTopics);
+  }, [listTopics])
+
   return (
     <View style={styles.styleCenter}>
       <Text>Play</Text>
