49e049d2f524515d6f9b02b98b8358858538c3b0
hellanngel <huynhtkthao@gmail.com>
2021-06-18 02:05:17 +0700

    topic screen

--- AnnaEnglish/Screens/TopicScreen/TopicScreen.js
+++ AnnaEnglish/Screens/TopicScreen/TopicScreen.js
@@ -1,31 +1,80 @@
-import React from "react";
-import { View, SafeAreaView, Image, Text, StyleSheet, TouchableOpacity } from "react-native";
+import React, { useEffect, useRef, useState } from "react";
+import {
+  View,
+  SafeAreaView,
+  StyleSheet,
+  TouchableOpacity,
+  Image,
+  ScrollView,
+  FlatList,
+  Text,
+} from "react-native";
 import { colors } from "../../config/colors";
 import { useRealtimeFire } from "../../hooks/useRealtimeFire";
+import Header from "../../components/header/header";
+import { styles } from "./styles";
+import Fire from "../../firebase/Fire";
 
 export default TopicScreen = ({ route }) => {
-  const [topic,] = useRealtimeFire("topic", route?.params?.topicId)
+  const [topic] = useRealtimeFire("topic", route?.params?.topicId);
+  const [games, setGames] = useState([]);
+  const tempGames = useRef([]);
+  useEffect(() => {
+    if (topic?.games) {
+      Object.values(topic?.games)?.forEach((gameId) => {
+        Fire.get(`game/${gameId}`).then((game) => {
+          tempGames.current.push(game);
+          if (tempGames.current.length === Object.values(topic?.games)?.length)
+            setGames(tempGames.current);
+        });
+      });
+    }
+  }, [topic]);
 
-
+  const Card = ({ game }) => {
+    return (
+      <TouchableOpacity
+        style={{ flex: 1 }}
+        onPress={() => {
+          //cho nay ne thyyyycute , truyen cai game vo cho no :> //
+        }}
+      >
+        <View
+          style={[
+            styles.card,
+            {
+              backgroundColor: colors.gray,
+              marginBottom: 5,
+              margin: 5,
+            },
+          ]}
+        >
+          <Text style={[styles.label, { color: topic.labelColor }]}>
+            {game.toJSON().name}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  };
 
   return (
-    <SafeAreaView
-      style={{
-        flex: 1,
-        backgroundColor: colors.white,
-      }}
-    >
-      <TouchableOpacity style={{ marginTop: 50 }}>
-        <Text
-          style={{
-            flex: 1,
+    <SafeAreaView style={styles.container}>
+      <View style={{ marginTop: 30 }}>
+        <Header title={topic?.name} />
+      </View>
+
+      <ScrollView contentContainerStyle={styles.scrollViewWrapper}>
+        <FlatList
+          showsVerticalScrollIndicator={false}
+          contentContainerStyle={{
+            paddingBottom: 40,
           }}
-        >
-          To game
-        </Text>
-      </TouchableOpacity>
+          data={games}
+          renderItem={({ item }) => {
+            return <Card game={item} />;
+          }}
+        />
+      </ScrollView>
     </SafeAreaView>
   );
 };
-
-const style = StyleSheet.create({});
---
+++ AnnaEnglish/components/header/header.js
@@ -0,0 +1,48 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Ionicons from "react-native-vector-icons/Ionicons";
+import { useNavigation } from "@react-navigation/core";
+import { SCREENS } from "../../Screens";
+import { colors } from "../../config/colors";
+
+export default function Header({ title }) {
+  const navigation = useNavigation();
+  const openMenu = () => {
+    navigation.navigate(SCREENS.mainApp.name);
+  };
+
+  return (
+    <View style={styles.header}>
+      <Ionicons
+        name="arrow-back-circle"
+        size={28}
+        onPress={openMenu}
+        style={styles.icon}
+      />
+      <View>
+        <Text style={styles.headerText}>{title}</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  header: {
+    width: "100%",
+    height: 50,
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
+    backgroundColor: colors.facebookBg,
+  },
+  headerText: {
+    fontWeight: "bold",
+    fontSize: 20,
+    color: "#333",
+    letterSpacing: 1,
+  },
+  icon: {
+    position: "absolute",
+    left: 16,
+  },
+});
