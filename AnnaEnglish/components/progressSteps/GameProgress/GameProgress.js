46bb52c3607e3299ccd8b75beac02e074a8d5552
CuteTN <quantiennghia001@gmail.com>
2021-06-18 02:01:54 +0700

    Import react-native-progress-steps

---
+++ AnnaEnglish/components/progressSteps/GameProgress/GameProgress.js
@@ -0,0 +1,16 @@
+import React, { useMemo } from 'react';
+import { ProgressStep, ProgressSteps } from 'react-native-progress-steps';
+
+export default GameProgress = ({ currentStep = 0, maxSteps }) => {
+  const tempArray = useMemo(() => {
+    return new Array(maxSteps);
+  }, [maxSteps])
+
+  return (
+    <ProgressSteps>
+      {tempArray.map(t =>
+        <ProgressStep />
+      )}
+    </ProgressSteps>
+  )
+}
