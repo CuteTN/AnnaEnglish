1c3a77cf092b488d0651ee7b2688ee8cad750bf1
CuteTN <quantiennghia001@gmail.com>
2021-07-13 14:01:09 +0700

    Fix dictionary lost code

--- AnnaEnglish/Screens/DictionaryScreen/DictionaryScreen.js
+++ AnnaEnglish/Screens/DictionaryScreen/DictionaryScreen.js
@@ -13,18 +13,24 @@ import { colors } from "../../config/colors";
 import { useNavigation } from "@react-navigation/core";
 import { SCREENS } from "..";
 import { styles } from "./styles";
+import { useSignedIn } from "../../hooks/useSignedIn";
 
 function DictionaryScreen() {
   const vocabulary = useFiredux("vocabulary") ?? {};
+  const { user } = useSignedIn();
   const navigation = useNavigation();
   const [txtSearch, setTxtSearch] = useState("");
 
-  const listVocabulary = React.useMemo(() =>
-    Object.entries(vocabulary).map((entry) => ({
-      _id: entry[0],
-      ...entry[1],
-    }))
-  );
+  const listVocabulary = React.useMemo(() => {
+    const userVocabulary = Object.values(user?.progress?.vocabulary ?? {});
+
+    return Object.entries(vocabulary)
+      .filter(entry => userVocabulary.includes(entry[0]))
+      .map((entry) => ({
+        _id: entry[0],
+        ...entry[1],
+      }))
+  }, [vocabulary, user?.progress?.vocabulary]);
 
   const handleSelectWord = (word) => {
     navigation.navigate(SCREENS.word.name, { word });
@@ -42,7 +48,7 @@ function DictionaryScreen() {
       <View style={styles.header}>
         <View style={styles.wrapperInput}>
           <AntDesign name="search1" size={18} color="gray" />
-          <TextInput style={styles.inputText} onChangeText={setTxtSearch} />
+          <TextInput style={styles.inputText} onChangeText={setTxtSearch} placeholder={"Tìm kiếm từ vựng"} />
         </View>
       </View>
       <ScrollView contentContainerStyle={styles.scrollViewWrapper}>
